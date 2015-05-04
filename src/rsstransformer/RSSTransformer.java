8ee54e8cd8d27baba4797a3e6dd338f2525f447b
filin <filin@192.168.0.101>
2015-05-04 12:58:37 +0300

    First test load rss.

--- src/rsstransformer/RSSTransformer.java
+++ src/rsstransformer/RSSTransformer.java
@@ -6,9 +6,12 @@ package rsstransformer;
  */
 public class RSSTransformer {
 
+    public static final String LINK_1  = "http://www.cnews.ru/inc/rss/spo_rss.xml";
+    public static final String LINK_2  = "http://lenta.ru/rss/articles/";
+    public static final String LINK_3  = "http://www.vesti.ru/vesti.rss";
+
     public static void main(String[] args) throws Exception {
+        RSSLoader rss = new RSSLoader(LINK_1);
 
     }
 }
-
-
