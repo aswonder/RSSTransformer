0fbe661da167349a6a963483be3bb7ca8858b7ca
aswonder <F8Gkjo13>
2015-05-04 16:21:00 +0300

    Add package datamodel

---
+++ src/rsstransformer/datamodel/ChannelInfo.java
@@ -0,0 +1,47 @@
+package rsstransformer.datamodel;
+
+/**
+ * Created by Andrey S. Divov on 04.05.2015.
+ */
+public class ChannelInfo {
+    private String language = "";
+    private String title = "";
+    private String description = "";
+    private String link = "";
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+
+
+
+}
