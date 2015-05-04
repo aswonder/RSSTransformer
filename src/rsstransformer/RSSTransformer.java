671270dd43c7231cfece396f6330d4f24538ed15
filin <filin@192.168.0.101>
2015-05-04 21:02:58 +0300

    Configure to HTML create.

--- src/rsstransformer/RSSTransformer.java
+++ src/rsstransformer/RSSTransformer.java
@@ -2,6 +2,7 @@ package rsstransformer;
 
 import rsstransformer.interfaces.LoadDataInterface;
 import rsstransformer.interfaces.MakerInterface;
+import rsstransformer.makers.HTMLMaker;
 import rsstransformer.makers.TextMaker;
 
 /**
@@ -13,11 +14,11 @@ public class RSSTransformer {
     private static final String LINK_1  = "http://www.cnews.ru/inc/rss/spo_rss.xml";
     private static final String LINK_2  = "http://lenta.ru/rss/articles/";
     private static final String LINK_3  = "http://www.vesti.ru/vesti.rss";
-    private static final String FILE_NAME = "out.txt";
+    private static final String FILE_NAME = "out.html";
 
     public static void main(String[] args) throws Exception {
         LoadDataInterface rss = new RSSLoader(LINK_1);
-        MakerInterface maker = new TextMaker(rss.LoadData());
+        MakerInterface maker = new HTMLMaker(rss.LoadData());
         maker.make(FILE_NAME);
     }
 }
