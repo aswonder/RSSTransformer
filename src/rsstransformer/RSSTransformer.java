b2258ffe03f26a4bd1d5754f8074c16c5e101cca
filin <filin@192.168.0.103>
2015-05-07 10:44:11 +0300

    Optimize code.

--- src/rsstransformer/RSSTransformer.java
+++ src/rsstransformer/RSSTransformer.java
@@ -3,6 +3,7 @@ package rsstransformer;
 import rsstransformer.interfaces.LoadDataInterface;
 import rsstransformer.interfaces.MakerInterface;
 import rsstransformer.makers.HTMLMaker;
+import rsstransformer.makers.PDFMaker;
 import rsstransformer.makers.TextMaker;
 
 /**
@@ -14,11 +15,14 @@ public class RSSTransformer {
     private static final String LINK_1  = "http://www.cnews.ru/inc/rss/spo_rss.xml";
     private static final String LINK_2  = "http://lenta.ru/rss/articles/";
     private static final String LINK_3  = "http://www.vesti.ru/vesti.rss";
-    private static final String FILE_NAME = "out.html";
+    private static final String LINK_4  = "http://english.pravda.ru/world/export.xml";
+    private static final String TEXT_FILE_NAME = "out.txt";
+    private static final String HTML_FILE_NAME = "out.html";
+    private static final String PDF_FILE_NAME = "out.pdf";
 
     public static void main(String[] args) throws Exception {
-        LoadDataInterface rss = new RSSLoader(LINK_1);
-        MakerInterface maker = new HTMLMaker(rss.LoadData());
-        maker.make(FILE_NAME);
+        LoadDataInterface rss = new RSSLoader(LINK_4);
+        MakerInterface maker = new PDFMaker(rss.LoadData());
+        maker.make(PDF_FILE_NAME);
     }
 }
