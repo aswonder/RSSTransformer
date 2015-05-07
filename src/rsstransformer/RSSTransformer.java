111f612941fae953f17e6fd4ca539d7e2915bbbe
aswonder <a.s.divov@mail.ru>
2015-05-07 15:11:02 +0400

    Configure to create text, HTML and PDF.

--- src/rsstransformer/RSSTransformer.java
+++ src/rsstransformer/RSSTransformer.java
@@ -7,22 +7,29 @@ import rsstransformer.makers.PDFMaker;
 import rsstransformer.makers.TextMaker;
 
 /**
- *
  * @author Andrey S. Divov
  */
 public class RSSTransformer {
 
-    private static final String LINK_1  = "http://www.cnews.ru/inc/rss/spo_rss.xml";
-    private static final String LINK_2  = "http://lenta.ru/rss/articles/";
-    private static final String LINK_3  = "http://www.vesti.ru/vesti.rss";
-    private static final String LINK_4  = "http://english.pravda.ru/world/export.xml";
-    private static final String TEXT_FILE_NAME = "out.txt";
-    private static final String HTML_FILE_NAME = "out.html";
-    private static final String PDF_FILE_NAME = "out.pdf";
+    private static final String LINK_1 = "http://www.cnews.ru/inc/rss/spo_rss.xml";
+    private static final String LINK_2 = "http://lenta.ru/rss/articles/";
+    private static final String LINK_3 = "http://www.vesti.ru/vesti.rss";
+    private static final String LINK_4 = "http://english.pravda.ru/world/export.xml";
+
+    private static final String TEXT_FILE_NAME = "output.txt";
+    private static final String HTML_FILE_NAME = "output.html";
+    private static final String PDF_FILE_NAME = "output.pdf";
 
     public static void main(String[] args) throws Exception {
         LoadDataInterface rss = new RSSLoader(LINK_3);
-        MakerInterface maker = new PDFMaker(rss.LoadData());
-        maker.make(PDF_FILE_NAME);
+
+        MakerInterface rssText = new TextMaker(rss.LoadData());
+        rssText.make(TEXT_FILE_NAME);
+
+        MakerInterface rssHTML = new HTMLMaker(rss.LoadData());
+        rssHTML.make(HTML_FILE_NAME);
+
+        MakerInterface rssPDF = new PDFMaker(rss.LoadData());
+        rssPDF.make(PDF_FILE_NAME);
     }
 }
